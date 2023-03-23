22b697d757f0689f50efc5ea74fca49b72f73f2c
Websiteok <websiteok@Websiteoks-iMac.station>
2023-03-23 21:26:43 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,14 +5,6 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
-}
-
-module.exports = {
-  // Inne istniejące opcje konfiguracyjne...
-  exportPathMap: async function () {
-    return {
-      "/": { page: "/" },
-      // Dodaj więcej ścieżek, jeśli masz inne strony
-    };
-  },
 };
+
+module.exports = nextConfig;
